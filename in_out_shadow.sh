125663be1a917350465f1e948c3209df85e3b6f5
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-03-20 12:06:29 +0100

    update

--- in_out_shadow.sh
+++
@@ -1,3 +0,0 @@
-WORK=/home/vk/FAIRDROP_BW_MASTER
-#for i in 0.1 0.4 0.6 1.0;do cat $WORK/FAIRDROP_BW_$i/plots/in_out.dat | awk -v a=$i '{print a"\t"$1"\t"$2}';done
-for i in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;do cat $WORK/SHADOW_FAIRDROP_BW_$i/plots/in_out.dat | awk -v a=$i '{print a"\t"$1"\t"$2}';done
