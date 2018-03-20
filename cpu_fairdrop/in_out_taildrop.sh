38d84b926e236aac0d398530e810ee23d68fd7e5
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-03-20 15:47:07 +0100

    update

--- cpu_fairdrop/in_out_taildrop.sh
+++ cpu_fairdrop/in_out_taildrop.sh
@@ -1,2 +1,2 @@
-WORK=/home/vk/FAIRDROP_CPU_MASTER
+WORK=$(pwd)
 i=350;while [[ $i -le 7000 ]];do cat $WORK/NOFAIRDROP_CPU_$i/plots/in_out.dat | awk -v a=$i '{print a"\t"$1"\t"$2}';i=$(( $i+350 ));done
